382358049b12b439c8cf1811dd5023e066c32e68
Luke Pham <toanpv.fami@gmail.com>
2021-04-03 11:21:19 +0700

    init

---
+++ src/main/java/vn/lukepham/epethealth/EpethealthApplication.java
@@ -0,0 +1,11 @@
+package vn.lukepham.epethealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpethealthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EpethealthApplication.class, args);
+	}
+}
---
+++ src/test/java/vn/lukepham/epethealth/EpethealthApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.lukepham.epethealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpethealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
